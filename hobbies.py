3cce7f543b3bc979402f0c16e4cce0f0b44d68b3
shaamilshan <shmlvk123@gmail.com>
2025-08-20 16:46:15 +0530

    proggram 2

---
+++ hobbies.py
@@ -0,0 +1,10 @@
+shamilHobby = {"Design","Gaming","Playing"}
+vishnuHobby = {"Football","Gaming","Gym"}
+
+intersection = shamilHobby.intersection(vishnuHobby)
+union = shamilHobby.union(vishnuHobby)
+unique_hobbies = shamilHobby.symmetric_difference(vishnuHobby)
+
+print("Intersection:", intersection)
+print("Union:", union)
+print("unique_hobbies:", unique_hobbies)
